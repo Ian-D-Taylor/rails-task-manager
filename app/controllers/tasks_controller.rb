4d217e584b89df39b26b3f41095331fe7e909cd7
iantaylor <iantaylor03@gmail.com>
2021-02-20 18:30:24 +1100

    added all mvc yet to complete delete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,52 @@
-class TaskController < ApplicationController
-  #read
-  def read
-      
-  end
-  
+class TasksController < ApplicationController
+ def index
+    @tasks = Task.all 
+ end
+ def show 
+    @task = Task.find(params[:id])   
+ end
+ 
+
+ def new
+     @tasks = Task.new
+ end
+ def create
+     @task = Task.new(task_params)
+     @task.save
+     redirect_to tasks_url
+
+ end
+
+ def edit
+     @tasks = Task.find(params[:id])
+ end
+ 
+ def update
+     @task = Task.update(task_params)
+     redirect_to task_url(@task)
+ end
+ 
+ def destroy
+    @task.destroy
+    redirect_to task_url
+     
+ end
+#  def completed_task
+#     @task.completed = true
+#     @task.save 
+#     redirect_to tasks_path
+#  end
+ 
+ 
+ private
+
+ def task_params
+    params.require(:task).permit(:title, :details)
+     
+ end
+ 
+ 
+ 
+ 
+ 
 end
